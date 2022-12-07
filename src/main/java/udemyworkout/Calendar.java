5172edc91aa2a02cb5fd873c4b448b18254f896a
pari <parishelva@gmail.com>
2022-12-07 20:20:14 +0000

    Gitsample project on 07/12/2022

---
+++ src/main/java/udemyworkout/Calendar.java
@@ -0,0 +1,21 @@
+package udemyworkout;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Calendar {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.exe",
+				"C:\\Users\\44787\\eclipse-workspace\\Selenium_TestNG\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.path2usa.com/travel-companion/");
+		Thread.sleep(5000);
+		
+	}
+
+}
