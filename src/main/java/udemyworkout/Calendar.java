eeb177883e58fbf4b30cbe494b50fe6f7ea1ed48
pari <parishelva@gmail.com>
2022-12-07 20:52:42 +0000

    added selecting month code

--- src/main/java/udemyworkout/Calendar.java
+++ src/main/java/udemyworkout/Calendar.java
@@ -2,8 +2,12 @@ package udemyworkout;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Calendar {
 
@@ -15,8 +19,22 @@ public class Calendar {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://www.path2usa.com/travel-companion/");
 		Thread.sleep(5000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,900)");
 		Thread.sleep(5000);
-		
+		driver.findElement(By.xpath("//input[@id='form-field-travel_comp_date']")).click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+		wait.until(ExpectedConditions
+				.visibilityOf(driver.findElement(By.xpath("//div[@class='flatpickr-current-month']"))));
+
+		while (!driver.findElement(By.cssSelector("span.cur-month")).getText().contains("March")) {
+			System.out.println(driver.findElement(By.cssSelector("span.cur-month")).getText());
+			driver.findElement(By.xpath("//body/div/div/span[2]//*[name()='svg']")).click();
+			Thread.sleep(2000);
+			
+		}
+
 	}
 
 }
