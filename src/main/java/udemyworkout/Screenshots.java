25801dabc31e8fab8d5bda80e6529912e6f0ba74
pari <parishelva@gmail.com>
2022-12-08 18:30:03 +0000

    Added screenshots

---
+++ src/main/java/udemyworkout/Screenshots.java
@@ -0,0 +1,29 @@
+package udemyworkout;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Screenshots {
+
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.exe", "C:\\Users\\44787\\eclipse-workspace\\Selenium_TestNG\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.matalan.co.uk/");
+		
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src,new File("C:\\Users\\44787\\eclipse-workspace\\GitSample\\Screenshots\\Matalan.png"));
+		
+		driver.close();
+
+	}
+
+}
